22e059ff46c61521a648af8b6ba5497128876012
lou <lou@lou.com>
2022-05-24 15:00:39 +0800

    feat: add go pool

---
+++ pkg/gopool/pool.go
@@ -0,0 +1,124 @@
+package gopool
+
+import (
+	"context"
+	"sync"
+	"sync/atomic"
+)
+
+type Pool interface {
+	Name() string
+	SetCap(cap int32)
+	Go(f func())
+	CtxGo(ctx context.Context, f func())
+	SetPanicHandler(f func(context.Context, interface{}))
+}
+
+var taskPool sync.Pool
+
+func init() {
+	taskPool.New = newTask
+}
+
+type task struct {
+	ctx  context.Context
+	f    func()
+	next *task
+}
+
+func (t *task) zero() {
+	t.ctx = nil
+	t.f = nil
+	t.next = nil
+}
+
+func (t *task) Recycle() {
+	t.zero()
+	taskPool.Put(t)
+}
+
+func newTask() interface{} {
+	return &task{}
+}
+
+type taskList struct {
+	sync.Mutex
+	taskHead *task
+	taskTail *task
+}
+
+type pool struct {
+	name string
+	cap  int32
+
+	config *Config
+
+	taskHead  *task
+	taskTail  *task
+	taskLock  sync.Mutex
+	taskCount int32
+
+	workerCount int32
+
+	panicHandler func(context.Context, interface{})
+}
+
+func NewPool(name string, cap int32, config *Config) Pool {
+	p := &pool{
+		name:   name,
+		cap:    cap,
+		config: config,
+	}
+	return p
+}
+
+func (p *pool) Name() string {
+	return p.name
+}
+
+func (p *pool) SetCap(cap int32) {
+	atomic.StoreInt32(&p.cap, cap)
+}
+
+func (p *pool) Go(f func()) {
+	p.CtxGo(context.Background(), f)
+}
+
+func (p *pool) CtxGo(ctx context.Context, f func()) {
+	t := taskPool.Get().(*task)
+	t.ctx = ctx
+	t.f = f
+	p.taskLock.Lock()
+	if p.taskHead == nil {
+		p.taskHead = t
+		p.taskTail = t
+	} else {
+		p.taskTail.next = t
+		p.taskTail = nil
+	}
+	p.taskLock.Unlock()
+	atomic.AddInt32(&p.taskCount, 1)
+
+	if (atomic.LoadInt32(&p.taskCount) >= p.config.ScaleThreshold && p.workerCount < atomic.LoadInt32(&p.cap)) || p.workerCount == 0 {
+		p.incWorkerCount()
+		w := workerPool.Get().(*worker)
+		w.pool = p
+		w.run()
+	}
+}
+
+func (p *pool) SetPanicHandler(f func(context.Context, interface{})) {
+	p.panicHandler = f
+}
+
+func (p *pool) WorkerCount() int32 {
+	return atomic.LoadInt32(&p.workerCount)
+}
+
+func (p *pool) incWorkerCount() {
+	atomic.AddInt32(&p.workerCount, 1)
+}
+
+func (p *pool) decWorkerCount() {
+	atomic.AddInt32(&p.workerCount, -1)
+}
